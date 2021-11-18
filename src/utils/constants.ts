af250a78b200236cf2dc2892cf341fe87e03caf8
wenyuangui <wenyuangui@yeah.net>
2021-11-18 16:29:19 +0800

    update mock usd(eth-usd pair) address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const MOCK_ETHEREUM_ADDRESS = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
-export const MOCK_USD_ADDRESS = "0x10f7fc1f91ba351f9c629c5947ad69bd03c05b96";
+export const MOCK_ETHEREUM_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+export const MOCK_USD_ADDRESS = "0x9ceB4d4C184d1786614a593a03621b7F37F8685F";
 export const PROPOSAL_STATUS_INITIALIZING = "Initializing";
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
