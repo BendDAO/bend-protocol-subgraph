fed0e12aad80cdd97a4a8b37a06a5cef41a5c1c7
Thor Seldon <thorseldon@protonmail.com>
2022-05-22 14:54:27 +0800

    add paused time for liquidation

--- src/mapping/address-provider-registry.ts
+++ src/mapping/address-provider-registry.ts
@@ -6,6 +6,7 @@ import {
   AddressesProviderUnregistered,
 } from "../../generated/LendPoolAddressesProviderRegistry/LendPoolAddressesProviderRegistry";
 import { getProtocol } from "../helpers/initializers";
+import { zeroBI } from "../utils/converters";
 
 export function handleAddressesProviderRegistered(event: AddressesProviderRegistered): void {
   let protocol = getProtocol();
@@ -15,6 +16,8 @@ export function handleAddressesProviderRegistered(event: AddressesProviderRegist
     pool.protocol = protocol.id;
     pool.active = true;
     pool.paused = false;
+    pool.pauseStartTime = zeroBI();
+    pool.pauseDurationTime = zeroBI();
     pool.lastUpdateTimestamp = event.block.timestamp.toI32();
     pool.save();
 
